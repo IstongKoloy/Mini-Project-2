9b8781e8049633596a1e8a66fa5b9a57267b0954
Glenn Andaleon <andaleon34@gmail.com>
2023-01-20 09:30:24 +0800

    PMS

---
+++ js/action.js
@@ -0,0 +1,17 @@
+// Bootstrap tooltip activation
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function(){
+    myFunction()
+};
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
