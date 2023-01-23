b0780f0abcd36f293766b2b4ea2966af654d9c75
JTayone <118233605+JTayone@users.noreply.github.com>
2023-01-23 21:21:49 +0800

    API for testing
    API for testing

--- js/contact-action.js
+++ js/contact-action.js
@@ -3,14 +3,27 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 
 
-function SendMail(){
+function sendMail() {
    var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      contact: document.getElementById("contact").value,
-      message: document.getElementById("message").value,
-   }
-   emailjs.sned("service_hpk14cj","template_n92yw9k",params).then(function(res){
-      alert("Success!" +res.status);
-   })
-}
+     name: document.getElementById("name").value,
+     email: document.getElementById("email").value,
+     message: document.getElementById("message").value,
+   };
+ 
+   const serviceID = "service_hpk14cj";
+   const templateID = "template_n92yw9k";
+ 
+     emailjs.send(serviceID, templateID, params)
+     .then(res=>{
+         document.getElementById("name").value = "";
+         document.getElementById("email").value = "";
+         document.getElementById("contact").value = "";
+         document.getElementById("message").value = "";
+         console.log(res);
+         alert("Your message sent successfully!!")
+ 
+     })
+     .catch(err=>console.log(err));
+ 
+ }
+ 
