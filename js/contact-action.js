ce34b5a6bbb62a24c6221807b756fd6e3a4c3316
JTayone <118233605+JTayone@users.noreply.github.com>
2023-01-23 20:52:10 +0800

    edit jas API
    edit jas API

--- js/contact-action.js
+++ js/contact-action.js
@@ -3,9 +3,14 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 
 
-emailjs.sendForm('service_hpk14cj', 'template_n92yw9k', '#myForm')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+function SendMail(){
+   var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      contact: document.getElementById("contact").value,
+      message: document.getElementById("message").value,
+   }
+   emailjs.sned("service_hpk14cj","template_n92yw9k",params).then(function(res){
+      alert("Success!" +res.status);
+   })
+}
