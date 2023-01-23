dfe05f1c4cfdbaa40426199f019a52647c0a1614
JTayone <118233605+JTayone@users.noreply.github.com>
2023-01-23 16:44:07 +0800

    origin/Jas

--- js/contact-action.js
+++ js/contact-action.js
@@ -1,2 +1,11 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+emailjs.sendForm('service_hpk14cj', 'template_n92yw9k', '#myForm')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
