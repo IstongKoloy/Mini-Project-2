3896dc8f324b1e2bbc917fc3220288763c96ff97
Glenn Andaleon <andaleon34@gmail.com>
2023-01-24 20:46:52 +0800

    Delete jquery.js

--- js/jquery.js
+++
@@ -1,8 +0,0 @@
-// $(function(){
-//     $(document).on("click","#login", function(){
-//         // alert("test")
-//         // $(".modal-title").html("Login / Sign up");
-//         $(".modal-body").load("../pages/login_modal.html");
-//         $(".modal").modal("show");
-//     })
-// })
