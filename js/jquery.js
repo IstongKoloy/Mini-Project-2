d49845ec9d5d4aa1d7d02ded5a08647a634d9432
Glenn Andaleon <andaleon34@gmail.com>
2023-01-24 20:42:33 +0800

    too many edits T_T

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,8 @@
-$(function(){
-    $(document).on("click","#login", function(){
-        
-    })
-})
+// $(function(){
+//     $(document).on("click","#login", function(){
+//         // alert("test")
+//         // $(".modal-title").html("Login / Sign up");
+//         $(".modal-body").load("../pages/login_modal.html");
+//         $(".modal").modal("show");
+//     })
+// })
