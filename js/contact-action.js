9c4f82455df914f4248ea49e6fabea842fcfa5e5
JTayone <118233605+JTayone@users.noreply.github.com>
2023-01-24 11:23:09 +0800

    asd
    asd

--- js/contact-action.js
+++ js/contact-action.js
@@ -5,19 +5,20 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 function sendMail() {
    var params = {
-     name: document.getElementById("name").value,
-     email: document.getElementById("email").value,
-     message: document.getElementById("message").value,
-   };
+    fName: document.getElementById("fName").value,
+    email_Id: document.getElementById("email_Id").value,
+    contact_Id: document.getElementById("contact_Id").value,
+    message: document.getElementById("message").value,
+  };
  
    const serviceID = "service_hpk14cj";
    const templateID = "template_n92yw9k";
  
      emailjs.send(serviceID, templateID, params)
      .then(res=>{
-         document.getElementById("name").value = "";
-         document.getElementById("email").value = "";
-         document.getElementById("contact").value = "";
+         document.getElementById("fName").value = "";
+         document.getElementById("email_Id").value = "";
+         document.getElementById("contact_Id").value = "";
          document.getElementById("message").value = "";
          console.log(res);
          alert("Your message sent successfully!!")
