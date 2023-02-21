e2fba216f94c85416f322828183c4543897ba367
Glenn Andaleon <andaleon34@gmail.com>
2023-02-22 06:34:58 +0800

    logo's

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parking Management System</title>
+
+    <!-- custom CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+
+        <div class="wrap">
+            <div class="nav">
+                <a href="./" class="logo">Parking Management System</a>
+                <ul>
+                    <li><a href="./">Home</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+            <div class="main-wrapper">
+                <div class="content-wrapper">
+                    <div class="sidebar">
+                        <div class="sidebar-menu">
+                            <ul>
+                                <li><a href="#">Dashboard</a></li>
+                                <li><a href="#">Vehicle Category</a></li>
+                                <li><a href="#">Vehicle Entry</a></li>
+                                <li><a href="#"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="content">
+     
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="circle c1"></div>
+        <div class="circle c2"></div>
+        <div class="circle c3"></div>
+        <div class="circle c4"></div>
+    </div>
+
+</body>
+</html>
